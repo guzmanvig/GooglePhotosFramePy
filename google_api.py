65ec32e2d44d4df76abc7fce841cbc7a290dacbe
Guzman <guzmanvigliecca@gmail.com>
2024-07-30 14:30:40 -0300

    Using ids to get photos

--- google_api.py
+++ google_api.py
@@ -2,7 +2,7 @@ import asyncio
 import os.path
 import traceback
 from time import sleep
-
+from tenacity import retry, wait_random_exponential, stop_after_attempt
 import numpy as np
 import requests
 from google.auth.exceptions import RefreshError
@@ -13,7 +13,7 @@ from config import config
 
 SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
 
-all_photo_urls = []
+all_photo_ids = []
 
 
 def get_token():
@@ -75,62 +75,64 @@ def google_api_search(page_token):
     media_items = response_json["mediaItems"]
     next_page_token = response_json.get("nextPageToken")
 
-    photo_urls = [photo["baseUrl"] for photo in media_items]
-    return photo_urls, next_page_token
+    photo_ids = [photo["id"] for photo in media_items]
+    return photo_ids, next_page_token
 
 
 def get_all_media_items():
-    photo_urls, next_page_token = google_api_search("")
-    all_photo_urls.extend(photo_urls)
+    photo_ids, next_page_token = google_api_search("")
+    all_photo_ids.extend(photo_ids)
     page = 1
     while next_page_token:
         print(f"Getting Google Photos page {page}...")
-        photo_urls, next_page_token = google_api_search(next_page_token)
-        all_photo_urls.extend(photo_urls)
+        photo_ids, next_page_token = google_api_search(next_page_token)
+        all_photo_ids.extend(photo_ids)
         page += 1
     print("All photos have been retrieved.")
 
 
-def download_photo(photo_name, photo_url, all_photo_urls, retries=0):
-    if retries > 3:
-        raise ConnectionError(f"Error while downloading photo {photo_url}")
+@retry(wait=wait_random_exponential(min=3, max=5), stop=stop_after_attempt(3))
+def download_photo(photo_name, photo_id, all_photo_ids):
+    response = requests.get(f"https://photoslibrary.googleapis.com/v1/mediaItems/{photo_id}",
+                            headers={
+                                    "Authorization": f"Bearer {get_token()}"
+                                })
 
-    try:
-        response = requests.get(f"{photo_url}=d")
-        if response.status_code != 200:
-            print(f"Error {response.status_code} - {response.reason} while downloading photo {photo_url}, trying again...")
-            retries += 1
-            sleep(2)
-            download_photo(photo_name, photo_url, all_photo_urls, retries)
-        else:
-            with open(f"photos/{photo_name}.jpg", "wb") as f:
-                f.write(response.content)
-            all_photo_urls.remove(photo_url)
-    except ConnectionError:
-        traceback.print_exc()
-        retries += 1
-        sleep(2)
-        download_photo(photo_name, photo_url, all_photo_urls, retries)
+    if response.status_code != 200:
+        print(f"Error {response.status_code} - {response.reason} getting photo {photo_id}, trying again...")
+        raise ConnectionError(f"Error while getting photo {photo_id}")
+
+    data = response.json()
+    base_url = data["baseUrl"]
+    response = requests.get(f"{base_url}=d")
+
+    if response.status_code != 200:
+        print(f"Error {response.status_code} - {response.reason} downloading photo {photo_id}, trying again...")
+        raise ConnectionError(f"Error while getting photo {photo_id}")
+
+    with open(f"photos/{photo_name}.jpg", "wb") as f:
+        f.write(response.content)
+    all_photo_ids.remove(photo_id)
 
 
 async def download_random_photos(number_of_photos, photo_names):
     if len(photo_names) != number_of_photos:
         raise ValueError("The number of photo names should be equal to the number of photos")
 
-    if len(all_photo_urls) < number_of_photos:
-        all_photo_urls.clear()
+    if len(all_photo_ids) < number_of_photos:
+        all_photo_ids.clear()
         get_all_media_items()
 
     # Select random photos links
-    random_photos_urls = np.random.choice(all_photo_urls, number_of_photos, replace=False)
+    random_photos_ids = np.random.choice(all_photo_ids, number_of_photos, replace=False)
 
     # Create directory for photos if it doesn't exist
     if not os.path.exists('photos'):
         os.makedirs('photos')
 
     # Download the photos
-    for i, photo_url in enumerate(random_photos_urls):
-        download_photo(photo_names[i], photo_url, all_photo_urls)
+    for i, photo_id in enumerate(random_photos_ids):
+        download_photo(photo_names[i], photo_id, all_photo_ids)
 
 
 if __name__ == "__main__":
