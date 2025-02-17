e081638e06a7374260f86b82e1ff799fc9419a97
Guzman <guzmanvigliecca@gmail.com>
2025-02-17 17:07:50 -0300

    Killing app as well

--- linux_scripts/shut_down_and_suspend.sh
+++ linux_scripts/shut_down_and_suspend.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Kill all Python processes
+# Kill all Python and Node processes
 pkill -f "python"
+pkill -f "node"
 
 # Get wake-up timestamp for 9 AM tomorrow
 WAKE_TIME=$(date -d '09:00 tomorrow' +%s)
