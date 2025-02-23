6db74e67d59bfb27d94eb9504d290ba5175c2798
Guzman <guzmanvigliecca@gmail.com>
2025-02-23 11:28:18 -0300

    Fixed height

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ config = {
         'delay_between_photos': 600000,  # In milliseconds
         'transition_animation_duration': 500,  # In milliseconds
         'display_width': 1200,  # These 2 can be left as 0. Specify them only if you don't get fullscreen (happens in some devices).
-        'display_height': 1900,
+        'display_height': 1920,
         'pause': {
             'start': None,  # If set, the slideshow will pause at this time and will show a black screen. In windows it also lowers the brightness to 0.
             'end': None
